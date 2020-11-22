80c530357e47a46b988b569b632dfde1da6e36bd
goeltanu1986 <30600387+goeltanu1986@users.noreply.github.com>
2020-11-22 15:56:37 -0500

    Added new algorithm files

---
+++ GreenBookProb1.cpp
@@ -0,0 +1,9 @@
+//
+//  GreenBookProb1.cpp
+//  ChernoCpp
+//
+//  Created by Tanu Goel on 11/22/20.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+#include "GreenBookProb1.hpp"
---
+++ GreenBookProb1.hpp
@@ -0,0 +1,14 @@
+//
+//  GreenBookProb1.hpp
+//  ChernoCpp
+//
+//  Created by Tanu Goel on 11/22/20.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+#ifndef GreenBookProb1_hpp
+#define GreenBookProb1_hpp
+
+#include <stdio.h>
+
+#endif /* GreenBookProb1_hpp */
