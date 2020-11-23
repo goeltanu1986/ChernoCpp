f0eeb984eab6beafc0958f1b71aab5d3fde96687
goeltanu1986 <30600387+goeltanu1986@users.noreply.github.com>
2020-11-22 20:20:06 -0500

    Added a new module to work on algorithms from leetcode

---
+++ LeetCodeAlgos/LeetCodeAlgos/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LeetCodeAlgos
+//
+//  Created by Tanu Goel on 11/22/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
